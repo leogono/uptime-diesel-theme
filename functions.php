60b87b9fc23229e00be82a80315afd139a1c53cf
Leo Gono <hello@wpmeds.com>
2013-10-15 04:07:44 +0800

    major update on styles  and added shortcode

--- functions.php
+++ functions.php
@@ -8,9 +8,9 @@ define( 'CHILD_THEME_URL', 'http://www.leogono.com/' );
 define( 'CHILD_THEME_VERSION', '2.0.1' );
 
 //* Enqueue Lato Google font
-add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
-function genesis_sample_google_fonts() {
-	wp_enqueue_style( 'google-font-lato', '//fonts.googleapis.com/css?family=Lato:300,700', array(), CHILD_THEME_VERSION );
+add_action( 'wp_enqueue_scripts', 'genesis_google_fonts' );
+function genesis_google_fonts() {
+	wp_enqueue_style( 'google-font-lato', '//fonts.googleapis.com/css?family=Lato:400,900,400italic|Anton', array(), CHILD_THEME_VERSION );
 }
 
 //* Add HTML5 markup structure
@@ -24,3 +24,25 @@ add_theme_support( 'custom-background' );
 
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
+
+//* Remove the site description
+remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
+
+add_filter('widget_text', 'do_shortcode');
+
+//add shortcode with attribs
+function contact_s($atts, $content = null) {
+	extract(shortcode_atts(array(
+    'phone'	=> '0409 227 527',
+    'url'		=>	''
+	), $atts));
+	$contactUs .= '<div class="contact-us">';
+	$contactUs .= '<p>Call us now</p>';
+	$contactUs .= '<p class="tel">'.$phone.'</p>';
+	$contactUs .= '<p><a href="#" class="btn btn-contact">Click Here to Book Online</a></p>';
+	$contactUs .= '</div>';
+
+
+	return $contactUs;
+}
+add_shortcode('contact', 'contact_s');
